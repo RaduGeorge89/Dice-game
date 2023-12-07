06412c29bce974bfe99e2c2f1d7191d2ccaea66d
RaduGeorge89 <102859819+RaduGeorge89@users.noreply.github.com>
2023-12-07 16:34:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-function rollDice() {
-  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
-  document.querySelector(".img1").setAttribute("src", randomImageSource1);
+let player1Score = 0;
+        let player2Score = 0;
 
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-  document.querySelector(".img2").setAttribute("src", randomImageSource2);
+        function rollDice() {
+            var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+            var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+            document.querySelector(".img1").setAttribute("src", randomImageSource1);
 
-  if (randomNumber1 > randomNumber2) {
-      document.querySelector(".winner").innerHTML = "🚩 Player 1 Wins!";
-  } else if (randomNumber2 > randomNumber1) {
-      document.querySelector(".winner").innerHTML = "Player 2 Wins! 🚩";
-  } else {
-      document.querySelector(".winner").innerHTML = "Draw!";
-  }
-}
+            var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+            var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+            document.querySelector(".img2").setAttribute("src", randomImageSource2);
 
+            if (randomNumber1 > randomNumber2) {
+                document.querySelector(".winner").innerHTML = "🚩 Player 1 Wins!";
+                player1Score++;
+            } else if (randomNumber2 > randomNumber1) {
+                document.querySelector(".winner").innerHTML = "Player 2 Wins! 🚩";
+                player2Score++;
+            } else {
+                document.querySelector(".winner").innerHTML = "Draw!";
+            }
 
-document.getElementById("rollButton").addEventListener("click", rollDice);
+            // Update and display scores
+            document.getElementById("player1Score").textContent = player1Score;
+            document.getElementById("player2Score").textContent = player2Score;
+        }
 
+        // Add event listener to the button
+        document.getElementById("rollButton").addEventListener("click", rollDice);
 
-rollDice();
+        // Initial roll when the page loads
+        rollDice();
