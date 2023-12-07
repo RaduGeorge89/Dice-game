4f8e2929917d24a71f3afa4d53abaa533cdc8792
RaduGeorge89 <102859819+RaduGeorge89@users.noreply.github.com>
2023-12-07 16:28:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").setAttribute("src", randomImageSource1);
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").setAttribute("src", randomImageSource2);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector(".winner").innerHTML = "🚩 Player 1 Wins!";
+  if (randomNumber1 > randomNumber2) {
+      document.querySelector(".winner").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+      document.querySelector(".winner").innerHTML = "Player 2 Wins! 🚩";
+  } else {
+      document.querySelector(".winner").innerHTML = "Draw!";
+  }
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector(".winner").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector(".winner").innerHTML = "Draw!";
-}
+
+
+document.getElementById("rollButton").addEventListener("click", rollDice);
+
+
+rollDice();
